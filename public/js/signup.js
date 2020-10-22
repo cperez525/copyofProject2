ba5ea66be1fe7f6fbbdbdfc15788696447a9b1ed
Cris Perez <cristino.perez525@gmail.com>
2020-10-22 14:34:08 -0500

    Fixed login/signup error handling!

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,33 +1,44 @@
-$(document).ready(function() {
-    // Getting references to our form and input
-    var signUpForm = $("form.signup");
-    var emailInput = $("input#email-input");
-    var passwordInput = $("input#password-input");
+$(document).ready(function () {
+  // Getting references to our form and input
+  var signUpForm = $("form.signup");
+  var emailInput = $("input#email-input");
+  var passwordInput = $("input#password-input");
 
-    
-  
-    // When the signup button is clicked, we validate the email and password are not blank
-    signUpForm.on("submit", function(event) {
-      event.preventDefault();
-      var userData = {
-        email: emailInput.val().trim(),
-        password: passwordInput.val().trim()
-      };
-  
-      if (!userData.email || !userData.password) {
-        return;
-      }
 
-      $.post("api/signup", userData)
-        .then(function(data){
 
-          alert("User has been created. Only one user can currently exist in the system - please use the login credentials you provided to log into the existing account in the future.")
-        })
-        .fail(function(err){
-          alert("A User already exists in the system. Please use the existing login credentials to log into the account.")
-        })
-      
-      emailInput.val("");
-      passwordInput.val("");
-    });
+  // When the signup button is clicked, we validate the email and password are not blank
+  signUpForm.on("submit", function (event) {
+    event.preventDefault();
+    var userData = {
+      email: emailInput.val().trim(),
+      password: passwordInput.val().trim()
+    };
+
+    if (!userData.email || !userData.password) {
+      return;
+    }
+
+
+
+    // If we have an email and password, run the signUpUser function
+    signUpUser(userData.email, userData.password);
+    emailInput.val("");
+    passwordInput.val("");
   });
+  // Does a post to the signup route. If successful, we are redirected to the members page
+  // Otherwise we log any errors
+  function signUpUser(email, password) {
+    $.post("/api/signup", {
+      email: email,
+      password: password
+    })
+      .then(function (data) {
+        window.location.replace("/members");
+        // If there's an error, handle it by throwing up a bootstrap alert
+      })
+      .fail(function(err){
+        console.log(err);
+        alert("A user already exists in the system. Please use existing login credentials to log into the existing user account.")
+      });
+  }
+});
